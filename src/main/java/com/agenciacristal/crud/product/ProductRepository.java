3020b48f964fd9b073db16254b900520cce69e5d
AGENCIA CRISTAL APPS <christian.udemy@gmail.com>
2023-02-27 10:56:17 -0500

    first commit

---
+++ src/main/java/com/agenciacristal/crud/product/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.agenciacristal.crud.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findProductByName(String name);
+}
